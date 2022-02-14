472b6b5f403959fe51e4e9bcd046114ff7e597e4
DavidAlejandroSaenzMeneses <dasm17.as@gmail.com>
2022-02-14 17:34:48 -0500

    Correccion Bug DotenV
    Se realiza correcion de bug al aplicar import con la libreria dontev

--- src/dataBase/connection.js
+++ src/dataBase/connection.js
@@ -1,11 +1,7 @@
 import Sequelize from 'sequelize';
-import dotenv from 'dotenv';
+import 'dotenv/config';
 
-//dotenv configuration
-await dotenv.config({ path: '../../.env' });
-
-console.log(process.env.BD_USER);
-const connection = (new Sequelize(process.env.DB_NAME,'postgres','5b05b7717', {
+const connection = (new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASS, {
     host: process.env.DB_HOST,
     dialect: 'postgres',
     logging: false
