33d29831caa2ae630948e3224d9e4e17a98a08d8
DavidAlejandroSaenzMeneses <dasm17.as@gmail.com>
2022-02-15 18:14:26 -0500

    Ajuste Estructura Sequelize

--- src/models/Genre.js
+++ src/models/Genre.js
@@ -2,30 +2,24 @@
 const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
     class User extends Model {
-        /**
-         * Helper method for defining associations.
-         * This method is not a part of Sequelize lifecycle.
-         * The `models/index` file will call this method automatically.
-         */
         static associate(models) {
-            // define association here
         }
     }
     User.init({
-        id_author: {
+        id_genre: {
             type: DataTypes.INTEGER,
             primaryKey: true,
             autoIncrement: true,
             allowNull: false,
-            unique: 'id_author_unique'
+            unique: 'id_genre_unique'
         },
-        author_name: {
+        literary_genres_description: {
             type: DataTypes.STRING,
             allowNull: false
         }
     }, {
         sequelize,
-        modelName: 'author',
+        modelName: 'genre',
         timestamps: false
     });
     return User;
